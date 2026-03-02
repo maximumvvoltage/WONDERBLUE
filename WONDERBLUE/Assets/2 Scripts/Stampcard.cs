4fbf6198042057298b3493a2ba76109339863572
maximumvvoltage <146975484+maximumvvoltage@users.noreply.github.com>
2026-03-02 21:20:35 +0000

    water update, small park update

---
+++ WONDERBLUE/Assets/2 Scripts/Stampcard.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Stamcard : MonoBehaviour
+{
+    [Header("Waypoints")]
+    public GameObject stampCard;
+    public bool completed;
+    public TextAsset mawiriInteaction;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
